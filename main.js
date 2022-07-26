f1850d14ee682715317140ba633dbbff7276cb90
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-07-26 08:19:01 +0300

    Changes

--- main.js
+++ main.js
@@ -1,47 +1,58 @@
 
-const masyvas = []
+let masyvas = []
 
-const input = document.querySelector('input')
-const enterButton = document.querySelector('button[data-type="enterButton"]')
-const result = document.querySelector('#result')
-result.innerHTML = 'Krepšelyje kol kas neturite jokių prekių.'
+let inputItem = document.querySelector('input[data-type="#inputItem"]')
+let buttonEnter = document.querySelector('button[data-type="buttonEnter"]')
+let result = document.querySelector('#result')
+result.innerHTML = `<p class="text-center mt-4">Krepšelyje kol kas neturite jokių prekių!</p>`
 
-
-    const renderTable = (array) => {
-            if(masyvas.length > 0) {
-                let html = `<table class="table table-borderless d-flex justify-content-end mt-4">`
-                array.forEach((value, index) => {
-                    html += `<tr>
-                                <td data-type="tableData">
-                                    <span class="me-3">${value}</span>
-                                    <button onclick="removeItem(${index})" type="button" class="btn btn-outline-dark rounded-0">Ištrinti</button>
-                                </td>
-                            </tr>`
-                    })
-                
-                html += `     </tr>
-                        </table>`
-                
-                result.innerHTML = html        
-     
-            } else {
-                result.innerHTML = 'Krepšelyje kol kas neturite jokių prekių.'
-             }
-    }
-
-        // ENTER paspaudimo f-ja
-        enterButton.addEventListener('click', (event) => {
-            event.preventDefault()
-                masyvas.push(input.value)
-                console.log(masyvas)        
-                renderTable(masyvas)    
+const renderTable = (array) => {
+    
+    // console.log(array)
+    if(masyvas.length > 0) {
+        let html = `<table class="table table-borderless d-flex justify-content-end mt-4">
+                    <tbody>`
+                    
+        array.forEach((value, index) => {
+            html += `<tr>
+                        <td data-type="tableData">
+                            <span id="span" class="me-3">${value}</span>
+                            <button onclick="removeItem(${index})" type="button" class="btn btn-outline-dark rounded-0">Ištrinti</button>
+                        </td>
+                    </tr>`
         })
         
-        const removeItem = (index) => {
-            document.querySelectorAll('tr td')[index].remove()
-            // let i = 0
-            //     if(masyvas.indexOf(index) > -1)
-            //     masyvas.slice(index, 1)
+        html += `   </tbody>
+            </table>`
+
+
+        result.innerHTML = html
+
+    } else {
+        result.innerHTML = `<p class="text-center mt-4">Krepšelyje kol kas neturite jokių prekių!</p>`
+
         }
+}
 
 
+// ENTER button:
+buttonEnter.addEventListener('click', (event) => {
+    event.preventDefault()
+        if(inputItem.value >= 0 || inputItem === '') {
+            result.innerHTML += `<p class="text-center mt-4">Prekė nepasirinkta!</p>`
+            result.innerHTML += html
+        } else {
+            masyvas.push(inputItem.value)
+            renderTable(masyvas)
+            inputItem.value = ''
+   }
+})
+
+// DELETE button:
+const removeItem = (index) => {
+    console.log(index)
+    document.querySelectorAll('tr td')[index].remove()
+    
+    masyvas.splice(index, 1)
+    renderTable(masyvas)       
+}
